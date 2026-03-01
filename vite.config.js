44c6d0d96ccf8f6097e4daacc7c0c3e97dedbeae
MaggoKao <maggokao@gmail.com>
2026-03-01 12:45:36 +0800

    修正 vite 多頁面打包設定

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // 未來如果您新增了子頁面，請將它們取消註解
-        // blackDzambhala: resolve(__dirname, 'black-dzambhala.html'),
-        // ganapati: resolve(__dirname, 'ganapati.html'),
-        // yellowDzambhala: resolve(__dirname, 'yellow-dzambhala.html'),
-        // kurukulla: resolve(__dirname, 'kurukulla.html'),
-        // terms: resolve(__dirname, 'terms.html'),
+        black: resolve(__dirname, 'black-dzambhala.html'),
+        ganapati: resolve(__dirname, 'ganapati.html'),
+        yellow: resolve(__dirname, 'yellow-dzambhala.html'),
+        kurukulla: resolve(__dirname, 'kurukulla.html'),
+        // 如果您有新增 terms.html，請把下面這行的斜線 // 拿掉
+        // terms: resolve(__dirname, 'terms.html') 
       }
     }
   }
-})
+});
