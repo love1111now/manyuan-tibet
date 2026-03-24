8eb19db8b2cba46efcb3e77797f482385cdcf2a0
MaggoKao <maggokao@gmail.com>
2026-03-24 14:42:05 +0800

    chore: 加入 Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// 1. 導入 Analytics 組件 (注意是 /react)
+import { Analytics } from '@vercel/analytics/react' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* 2. 在 App 旁邊加上 Analytics 組件 */}
+    <Analytics /> 
   </StrictMode>,
 )
