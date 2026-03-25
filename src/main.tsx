cc50e4397e472ea9b635dda9520be0b5a97ae9a9
MaggoKao <maggokao@gmail.com>
2026-03-26 01:12:23 +0800

    fix: 修正 main.tsx 語法錯誤並補上 Vercel 追蹤

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,14 @@
-<!doctype html>
-<html lang="zh-TW">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>滿願藏庫｜法財滿願</title>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import './index.css'
+import App from './App.tsx'
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5EQ1SBXQ17"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-5EQ1SBXQ17');
-    </script>
-
-    <script>
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1974584759935803');
-      fbq('track', 'PageView');
-    </script>
-    <noscript>
-      <img height="1" width="1" style="display:none"
-           src="https://www.facebook.com/tr?id=1974584759935803&ev=PageView&noscript=1" />
-    </noscript>
-    </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.tsx"></script>
-  </body>
-</html>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </StrictMode>
+)
