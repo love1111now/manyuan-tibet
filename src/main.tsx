56695c1556e490f14a7720b047b37481fa2ed17d
MaggoKao <maggokao@gmail.com>
2026-03-25 16:16:26 +0800

    整合 Vercel Analytics 與 Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 1. 匯入 Vercel 追蹤組件
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* 2. 加入數據與效能監控元件 */}
+    <Analytics />
+    <SpeedInsights />
+    
     <App />
   </StrictMode>,
 )
