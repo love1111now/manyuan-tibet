b4417295f581dde2e7ffbed1be1c30acc9e00287
MaggoKao <maggokao@gmail.com>
2026-03-25 16:19:12 +0800

    修正：正式在 main.tsx 植入 Vercel Analytics 與 Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// 1. 匯入 Vercel 追蹤組件
+// 1. 加入 Vercel 數據與效能監控的匯入指令
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* 2. 加入數據與效能監控元件 */}
+    {/* 2. 在渲染流程中植入監控元件 */}
     <Analytics />
     <SpeedInsights />
     
